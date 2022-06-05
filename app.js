d27441178c8e9b208ecf9a217d871947ac487f6d
bazinygero <97780599+bazinygero@users.noreply.github.com>
2022-06-06 00:00:32 +0300

    facing some error in api, will dive into it tomorrow

---
+++ app.js
@@ -0,0 +1,128 @@
+const app = document.querySelector('.weather-app');
+const temp = document.querySelector('.temp');
+const dateOutput = document.querySelector('.date');
+const timeOutput = document.querySelector('.time');
+const conditionOutput = document.querySelector('.condition');
+const nameOutput = document.querySelector('.name');
+const icon = document.querySelector('.icon');
+const cloudOutput = document.querySelector('.cloud');
+const humidityOutput = document.querySelector('.humidity');
+const windOutput = document.querySelector('.wind');
+const form = document.querySelector('.location-input');
+const search = document.querySelector('.search');
+const cities = document.querySelectorAll('.city');
+const btn = document.querySelector('.submit');
+
+let cityInput = "London";
+
+
+cities.forEach((city) => {
+    city.addEventListener('click', (e) => {
+
+        cityInput = e.target.innerHTML;
+
+        fetchWeatherData();
+
+        app.style.opacity = "0";
+    })
+})
+
+form.addEventListener('submit', (e) => {
+
+    if(search.value.length == 0) {
+        alert('Please type in a city name');
+    } else {
+
+        cityInput = search.value;
+
+        fetchWeatherData();
+
+        search.value = "";
+        app.style.opacity = 0;
+    }
+    e.preventDefault();
+})
+
+function dayOfTheWeek(day, month, year) {
+    const weekday = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    return weekday[new Date(`${day}/${month}/${year}`).getDay()];
+}
+
+function fetchWeatherData() {
+fetch(`http://api.weatherapi.com/v1/current.json?key=f31cd0f949e143df86f202443220506&q=${cityInput}&aqi=no`)
+
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        temp.innerHTML = data.current.temp_c + "&#176";
+        conditionOutput.innerHTML = data.current.contition.text;
+
+        const date = data.location.localtime;
+        const y = parseInt(date.substr(0,4));
+        const m = parseInt(date.substr(5,2));
+        const d = parseInt(date.substr(8,2));
+        const time = date.substr(11);
+
+        dateOutput.innerHTML = `${dayOfTheWeek (d, m, y)} ${d}, ${m}, ${y}`
+        nameOutput.innerHTML = data.location.name;
+
+        const iconId = data.current.condition.icon.substr("//cdn.weatherapi.com/weather/64x64/".length);
+
+        icon.src = "./icons/" + iconId;
+
+        cloudOutput.innerHTML = data.current.cloud + "%";
+        humidityOutput.innerHTML = data.current.humidity + "%";
+        windOutput.innerHTML = data.current.wind_kph + "km/h";
+
+        let timeOfDay = "day";
+        
+        const code = data.current.condition.code;
+
+        if(!data.current.is_day) {
+            timeOfDay = "night";
+        }
+
+        if(code == 1000) {
+            app.style.backgroundImage = `url(./images/${timeOfDay}/clear.jpg)`;
+            btn.style.background = '#e5ba92';
+            if(timeOfDay == "night") {
+                btn.style.background = '#181e27';
+            }
+        } else if (
+            code == 1003 ||
+            code == 1006 ||
+            code == 1009 ||
+            code == 1030 ||
+            code == 1069 ||
+            code == 1087 ||
+            code == 1135 ||
+            code == 1273 ||
+            code == 1276 ||
+            code == 1279 ||
+            code == 1282 
+        ) {
+            app.style.backgroundImage = `url(./images/${timeOfDay}/cloudy.jpg)`;
+            btn.style.background = '#fa6d1b';
+            if (timeOfDay == "night") {
+                btn.style.background = '#181e27';
+            }
+        }
+    })
+    .catch (() => {
+        alert('City not found, please try again');
+        app.style.opacity = "1";
+    })
+}
+
+fetchWeatherData();
+
+app.style.opacity = "1";
