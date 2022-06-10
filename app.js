659170c8c2937491d712baae332eefcfc00df99f
bazinygero <97780599+bazinygero@users.noreply.github.com>
2022-06-11 00:35:53 +0300

    minor changes, error remains

--- app.js
+++ app.js
@@ -67,12 +67,13 @@ fetch(`http://api.weatherapi.com/v1/current.json?key=f31cd0f949e143df86f20244322
         conditionOutput.innerHTML = data.current.contition.text;
 
         const date = data.location.localtime;
-        const y = parseInt(date.substr(0,4));
-        const m = parseInt(date.substr(5,2));
-        const d = parseInt(date.substr(8,2));
+        const y = parseInt(date.substr(0, 4));
+        const m = parseInt(date.substr(5, 2));
+        const d = parseInt(date.substr(8, 2));
         const time = date.substr(11);
 
-        dateOutput.innerHTML = `${dayOfTheWeek (d, m, y)} ${d}, ${m}, ${y}`
+        dateOutput.innerHTML = `${dayOfTheWeek (d, m, y)} ${d}, ${m}, ${y}`;
+        timeOutput.innerHTML = time;
         nameOutput.innerHTML = data.location.name;
 
         const iconId = data.current.condition.icon.substr("//cdn.weatherapi.com/weather/64x64/".length);
@@ -115,7 +116,41 @@ fetch(`http://api.weatherapi.com/v1/current.json?key=f31cd0f949e143df86f20244322
             if (timeOfDay == "night") {
                 btn.style.background = '#181e27';
             }
+        } else if (
+            code == 1063 ||
+            code == 1069 ||
+            code == 1072 ||
+            code == 1150 ||
+            code == 1153 ||
+            code == 1180 ||
+            code == 1183 ||
+            code == 1186 ||
+            code == 1189 ||
+            code == 1192 ||
+            code == 1195 ||
+            code == 1204 ||
+            code == 1207 ||
+            code == 1240 ||
+            code == 1243 ||
+            code == 1246 ||
+            code == 1249 ||
+            code == 1252
+        ) {
+            app.style.backgroundImage = `url(./images/${timeOfDay}/rainy.jpg)`;
+            btn.style.background = '#647d75';
+            if(timeOfDay == "night") {
+                btn.style.background = '#325c80';
+            } 
+        } else {
+            app.style.backgroundImage = `
+              url(./images/${timeOfDay}/snowy.jpg)`;
+            btn.style.background="#4d72aa";
+            if(timeOfDay == "night") {
+              btn.style.background="#1b1b1b";
+            }
         }
+
+        app.style.opacity = "1";
     })
     .catch (() => {
         alert('City not found, please try again');
